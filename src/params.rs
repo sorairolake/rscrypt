c9bb8ac90bb5d127d7a7b985e29454d9b2503d35
Shun Sakai <sorairolake@protonmail.ch>
2023-01-27 16:20:04 +0900

    Fix Clippy errors

--- src/params.rs
+++ src/params.rs
@@ -92,10 +92,7 @@ fn display_with_resources(
     );
     display(n, r, p);
     eprintln!(" (available {mem_limit})");
-    eprint!(
-        "Takes about {:.2?} (limit: {:.2?})",
-        expected_secs, max_time
-    );
+    eprint!("Takes about {expected_secs:.2?} (limit: {max_time:.2?})");
 }
 
 /// Prints the encryption parameters with resources, with a newline.
